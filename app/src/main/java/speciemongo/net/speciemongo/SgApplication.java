0540aedbadfbc45440ac9782a31dfb5301982bc3
Johannes Klein <johannesklein@Johanness-MacBook-Air.local>
2016-08-10 15:31:59 +0200

    Enable strict mode in Debug builds

--- app/src/main/java/speciemongo/net/speciemongo/SgApplication.java
+++ app/src/main/java/speciemongo/net/speciemongo/SgApplication.java
@@ -1,9 +1,30 @@
 package speciemongo.net.speciemongo;
 
 import android.app.Application;
+import android.os.StrictMode;
 
 /**
- * Created by johannesklein on 10.08.16.
+ * The {@link Application} implementation for the app.
  */
 public class SgApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // If we are in debug mode,
+        if(BuildConfig.DEBUG) {
+
+            // Enable strict mode
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build());
+
+        }
+    }
 }
