0fd9656106e926e608f8b8527f87671e97a519ea
Johannes Klein <johannesklein@Johanness-MacBook-Air.local>
2016-08-10 15:11:21 +0200

    Basic project set-up

---
+++ app/src/main/java/speciemongo/net/speciemongo/SgActivityMain.java
@@ -0,0 +1,13 @@
+package speciemongo.net.speciemongo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SgActivityMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sg_main);
+    }
+}
