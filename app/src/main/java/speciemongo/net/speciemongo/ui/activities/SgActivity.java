9720ddabf10d87f47023b15ecbc7bdd9e5860bac
Johannes Klein <johannesklein@Johanness-MacBook-Air.local>
2016-08-10 15:43:29 +0200

    Added activity superclass

---
+++ app/src/main/java/speciemongo/net/speciemongo/ui/activities/SgActivity.java
@@ -0,0 +1,12 @@
+package speciemongo.net.speciemongo.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * A super class for all {@link SgActivity}s in the app.
+ */
+
+public class SgActivity extends AppCompatActivity {
+
+
+}
