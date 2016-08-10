ed7122e7ff74292d6161578797de93293f073bc7
Johannes Klein <johannesklein@Johanness-MacBook-Air.local>
2016-08-10 16:32:46 +0200

    Added a launcher activity

---
+++ app/src/main/java/speciemongo/net/speciemongo/ui/activities/SgActivityLauncher.java
@@ -0,0 +1,22 @@
+package speciemongo.net.speciemongo.ui.activities;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * The entry {@link AppCompatActivity} of the app. The user is forwarded either to the login screen
+ * or the main activity based on if he is logged in or not.
+ */
+
+// TODO This class pre-loads the profile repository to make the decision.
+
+public class SgActivityLauncher extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
