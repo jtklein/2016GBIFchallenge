f20011b9033dc3b4a25f12e7447eeebe233076b2
Johannes Klein <johannesklein@Johanness-MacBook-Air.local>
2016-08-11 00:08:25 +0200

    Basic method layout for requesting location permissions at runtime

--- app/src/main/java/speciemongo/net/speciemongo/ui/activities/SgActivityMap.java
+++ app/src/main/java/speciemongo/net/speciemongo/ui/activities/SgActivityMap.java
@@ -13,8 +13,45 @@ public class SgActivityMap extends SgActivityMain {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // If the user did not grant Location permissions request them
+        if (!this.hasLocationPermissions()) {
+            this.requestLocationPermissions();
+        }
+
         // Set Content View
         setContentView(R.layout.sg_activity_map);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
+
+    public Boolean hasLocationPermissions() {
+
+    }
+
+    public void requestLocationPermissions() {
+
 
     }
 }
