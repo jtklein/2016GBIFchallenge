f47bc888f0c82b59f014e9177314261c76bacdfd
Johannes Klein <johannesklein@Johanness-MacBook-Air.local>
2016-08-10 23:45:55 +0200

    Created dummy button to start map activity

--- app/src/main/java/speciemongo/net/speciemongo/ui/activities/SgActivityMain.java
+++ app/src/main/java/speciemongo/net/speciemongo/ui/activities/SgActivityMain.java
@@ -1,7 +1,10 @@
 package speciemongo.net.speciemongo.ui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import speciemongo.net.speciemongo.R;
 
 public class SgActivityMain extends SgActivity {
@@ -9,7 +12,20 @@ public class SgActivityMain extends SgActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set Content View
         setContentView(R.layout.sg_activity_main);
+
+        // Bind Views
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.buttonDummyToMap)
+    public void onDummyToMapClicked() {
+        Intent i = new Intent(this, SgActivityMap.class);
+        this.startActivity(i);
+        this.finish();
 
     }
 }
