f61dcae363569ee724f74ec80e2c26d7c3cda0b3
Johannes Klein <johannesklein@Johanness-MacBook-Air.local>
2016-08-10 19:09:08 +0200

    Bind Login Views

--- app/src/main/java/speciemongo/net/speciemongo/ui/activities/SgActivityLogin.java
+++ app/src/main/java/speciemongo/net/speciemongo/ui/activities/SgActivityLogin.java
@@ -1,7 +1,11 @@
 package speciemongo.net.speciemongo.ui.activities;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import speciemongo.net.speciemongo.R;
 
 /**
@@ -10,12 +14,24 @@ import speciemongo.net.speciemongo.R;
 
 public class SgActivityLogin extends SgActivity {
 
+    /**
+     * The {@link android.widget.EditText} for the email
+     */
+    @Bind(R.id.editTextEmail)
+    TextView mEditTextEmail;
+
+    @Bind(R.id.buttonLogin)
+    Button mButtonLogin;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // Set Content View
         this.setContentView(R.layout.sg_activity_login);
+
+        // Bind Views
+        ButterKnife.bind(this);
 
     }
 }
