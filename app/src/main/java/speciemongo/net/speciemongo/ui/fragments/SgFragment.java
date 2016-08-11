61780a4d6f17cb4b21f6149139096664908c16c4
Johannes Klein <johannesklein@Johanness-MacBook-Air.local>
2016-08-11 16:41:44 +0200

    Added custom fragment superclass

---
+++ app/src/main/java/speciemongo/net/speciemongo/ui/fragments/SgFragment.java
@@ -0,0 +1,29 @@
+package speciemongo.net.speciemongo.ui.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+/**
+ * A superclass for all {@link Fragment}s used in the app
+ */
+public class SgFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStop();
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+    }
+
+}
