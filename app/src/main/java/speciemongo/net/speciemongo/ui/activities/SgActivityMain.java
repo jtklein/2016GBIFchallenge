75b12b76fadf5420eea5eca2ce1588a676617010
Johannes Klein <johannesklein@Johanness-MacBook-Air.local>
2016-08-11 15:35:11 +0200

    Request location permissions at runtime

--- app/src/main/java/speciemongo/net/speciemongo/ui/activities/SgActivityMain.java
+++ app/src/main/java/speciemongo/net/speciemongo/ui/activities/SgActivityMain.java
@@ -1,13 +1,27 @@
 package speciemongo.net.speciemongo.ui.activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import speciemongo.net.speciemongo.R;
 
 public class SgActivityMain extends SgActivity {
+
+    /**
+     * The request code for the ACCESS_FINE_LOCATION permission
+     */
+    private final static int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1122;
+
+    /**
+     * The request code for the ACCESS_COARSE_LOCATION permission
+     */
+    private final static int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 2233;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +38,16 @@ public class SgActivityMain extends SgActivity {
     @OnClick(R.id.buttonDummyToMap)
     public void onDummyToMapClicked() {
 
-        // If the user did not grant Location permissions request them
-        if (!this.hasLocationPermissions()) {
-            this.requestLocationPermissions();
+        // If the user did not grant Fine Location permission, request it
+        if (!this.hasFineLocationPermission()) {
+            this.requestFineLocationPermissions();
+        }
+        // If the user did not grant Coarse Location permission, request it
+        if (!this.hasCoarseLocationPermission()) {
+            this.requestCoarseLocationPermissions();
         }
 
-        if (this.hasLocationPermissions()) {
+        if (this.hasFineLocationPermission() && this.hasCoarseLocationPermission()) {
             Intent i = new Intent(this, SgActivityMap.class);
             this.startActivity(i);
             this.finish();
@@ -37,13 +55,97 @@ public class SgActivityMain extends SgActivity {
         }
     }
 
-    public Boolean hasLocationPermissions() {
-        return true;
+    /**
+     * Checks if the app has the ACCESS_FINE_LOCATION permission
+     * @return true if permission is granted
+     */
+    public Boolean hasFineLocationPermission() {
+        // Check if we have fine location access
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) {
+            return true;
+
+        } else {
+            return false;
+
+        }
+    }
+
+    /**
+     * Checks if the app has the ACCESS_COARSE_LOCATION permission
+     * @return true if permission is granted
+     */
+    public Boolean hasCoarseLocationPermission() {
+        // Check if we have fine location access
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) {
+            return true;
+
+        } else {
+            return false;
+
+        }
+    }
+
+    /**
+     * Requests the ACCESS_FINE_LOCATION permission at runtime
+     */
+    public void requestFineLocationPermissions() {
+        // Request the permission.
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
 
     }
 
-    public void requestLocationPermissions() {
+    /**
+     * Requests the ACCESS_COARSE_LOCATION permission at runtime
+     */
+    public void requestCoarseLocationPermissions() {
+        // Request the permission.
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
 
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        // The results of the asking for permission dialogue
+        switch (requestCode) {
+            // Callback is from the ACCESS_FINE_LOCATION permission
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // ACCESS_FINE_LOCATION permission was granted
+                    // nothing to do here
+
+                } else {
+
+                    // ACCESS_FINE_LOCATION permission denied
+                    // TODO show explanation why we need it
+                }
+                return;
+            }
+            // Callback is from the ACCESS_COARSE_LOCATION permission
+            case MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // ACCESS_COARSE_LOCATION permission was granted
+                    // nothing to do here
+
+                } else {
+
+                    // ACCESS_COARSE_LOCATION permission denied
+                    // TODO show explanation why we need it
+                }
+                return;
+            }
+        }
     }
 }
