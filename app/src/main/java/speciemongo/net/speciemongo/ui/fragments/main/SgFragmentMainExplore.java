0d7320d1ed7af3eda6bd0f771a164a9cd2415eb2
Johannes Klein <johannesklein@Johanness-MacBook-Air.local>
2016-08-11 23:03:11 +0200

    Added class for the explore fragment

---
+++ app/src/main/java/speciemongo/net/speciemongo/ui/fragments/main/SgFragmentMainExplore.java
@@ -0,0 +1,23 @@
+package speciemongo.net.speciemongo.ui.fragments.main;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import speciemongo.net.speciemongo.R;
+import speciemongo.net.speciemongo.ui.fragments.SgFragment;
+
+/**
+ * The {@link Fragment} of the main activity that links to the explore activity
+ */
+public class SgFragmentMainExplore extends SgFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.sg_fragment_main_explore, container, false);
+    }
+}
