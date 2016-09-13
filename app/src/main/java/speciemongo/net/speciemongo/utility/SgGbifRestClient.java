c36409d003c0fa3197c0fb5883799a1eaade7408
Johannes Klein <johannesklein@Johanness-MacBook-Air.local>
2016-09-14 00:30:27 +0200

    Added utility class for RESTful calls to GBIF API

---
+++ app/src/main/java/speciemongo/net/speciemongo/utility/SgGbifRestClient.java
@@ -0,0 +1,28 @@
+package speciemongo.net.speciemongo.utility;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+/**
+ * Created by johannesklein on 13.09.16.
+ */
+
+public class SgGbifRestClient {
+
+    private static final String BASE_URL = "http://api.gbif.org/v1/";
+
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
+}
