7d7fee615f30b24e7ea262fb51292e82596ed796
Johannes Klein <johannesklein@Johanness-MacBook-Air.local>
2016-09-14 13:20:00 +0200

    Starting camera fragment from activity

--- app/src/main/java/speciemongo/net/speciemongo/ui/activities/SgActivityCamera.java
+++ app/src/main/java/speciemongo/net/speciemongo/ui/activities/SgActivityCamera.java
@@ -1,9 +1,29 @@
 package speciemongo.net.speciemongo.ui.activities;
 
-/**
- * Created by johannesklein on 11.08.16.
- */
+import android.os.Bundle;
 
+import speciemongo.net.speciemongo.R;
+import speciemongo.net.speciemongo.ui.fragments.camera.SgFragmentCamera;
+
+/**
+ * The activity that shows the camera element.
+ */
 public class SgActivityCamera extends SgActivityMain {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.sg_activity_camera);
+
+        // TODO add functionality for Android versions 15-19
+
+        if (null == savedInstanceState) {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.cameraContainer, SgFragmentCamera.newInstance())
+                    .commit();
+        }
+    }
+
 
 }
