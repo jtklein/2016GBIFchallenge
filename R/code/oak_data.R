a3e82e7679668ff650435b34f6d373fbce24589d
Tom August <tomaug@ceh.ac.uk>
2016-09-18 22:21:04 +0100

    Create code to model presence

--- R/code/oak_data.R
+++ R/code/oak_data.R
@@ -1,39 +1,43 @@
 # Get fine resolution data for a species
+rm(list = ls())
+
 library(rgbif)
-nrec = 1000
-layers = 1:3
+#nrec = 100000
+layers = 1:19 # up to 19
 
 # get the species code
 # returns numerous quercus rubras!
-oak <- name_suggest(q = 'quercus rubra',rank = 'species')
-
-oak_data <- occ_data(scientificName = 'quercus rubra', hasCoordinate = TRUE, limit = nrec)
-
-oak_data$meta
-nrow(oak_data$data)
-
-plot(oak_data$data$decimalLongitude, oak_data$data$decimalLatitude)
-
-# Only keep ones with good accuracy
-sum(is.na(oak_data$data$coordinateUncertaintyInMeters))
-d <- oak_data$data
-d <- d[!is.na(d$coordinateUncertaintyInMeters),]
-d <- d[d$coordinateUncertaintyInMeters <= 10, ]
-d$value <- 1
-
-write.csv(d[,c('decimalLatitude','decimalLongitude', 'value')], file = 'R/presence_data/oak_data.csv', row.names = FALSE)
+# oak <- name_suggest(q = 'quercus rubra',rank = 'species')
+# 
+# oak_data <- occ_data(scientificName = 'quercus rubra', hasCoordinate = TRUE, limit = nrec)
+# 
+# oak_data$meta
+# nrow(oak_data$data)
+# 
+# plot(oak_data$data$decimalLongitude, oak_data$data$decimalLatitude)
+# 
+# # Only keep ones with good accuracy
+# sum(is.na(oak_data$data$coordinateUncertaintyInMeters))
+# d <- oak_data$data
+# d <- d[!is.na(d$coordinateUncertaintyInMeters),]
+# d <- d[d$coordinateUncertaintyInMeters <= 10, ]
+# d$value <- 1
+# 
+# write.csv(d[,c('decimalLatitude','decimalLongitude', 'value')], file = 'R/presence_data/oak_data.csv', row.names = FALSE)
 
 library(zoon)
 
-w <- workflow(LocalOccurrenceData(filename = 'R/presence_data/oak_data.csv',
-                                  columns = c(long = "decimalLongitude", lat = "decimalLatitude", value = "value")),
-              Bioclim(layers = layers),
-              QuickGRaF,
-              LogisticRegression,
-              PrintMap)
+system.time({
+  w <- workflow(LocalOccurrenceData(filename = 'R/presence_data/oak_data.csv',
+                                    columns = c(long = "decimalLongitude", lat = "decimalLatitude", value = "value")),
+                Bioclim(layers = layers, res = 2.5),
+                Chain(Clean, Background(10000)),
+                RandomForest,
+                PrintMap)
+})
 
 save(w, file = 'R/zoon_temp.rdata')
 
-writeRaster(Output(w), filename = 'R/predicted_occurrence/oak_test.tif', format = 'GTiff')
+writeRaster(Output(w), filename = 'R/predicted_occurrence/oak_test.tif', format = 'GTiff', overwrite = TRUE)
 
 plot(Output(w))
